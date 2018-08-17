d81e0d84ba84494cb0d14b1d4c2288634ebadcb2
chibikun4 <freezynacromonger@gmail.com>
2018-08-17 09:30:13 +0545

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//This is mine. I coded this so don't copy. Ownership @Copyright. Copy garyo ki katta handichu.
+
+function SayHello() 
+{
+	alert("Good Morning All from external js. From onclick");
+}
+function SayBye()
+{
+	alert("Good Bye All from external js. From onload");
+}
+//SayHello() Pascal Case
+//sayHello() Camel Case
